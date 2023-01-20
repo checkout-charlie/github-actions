54d03d66eaf0a568e1bb298ef57e8dcdb243ba18
Federico Infanti <federico.infanti@checkout-charlie.com>
2023-01-20 15:56:27 +0100

    Add cleanup after image build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -21,19 +21,14 @@ fi
 if [ "$MULTI_STAGE" != "false" ]
 then
 	echo "Building image..."
-  docker build $BUILD_ARGS --target "$PRODUCTION_STAGE" -t "$IMAGE_NAME:$GITHUB_SHA" -t "$IMAGE_NAME:latest" $ADDITIONAL_TAG_COMMAND "$BUILD_CONTEXT" \
+  docker build $BUILD_ARGS --target "$PRODUCTION_STAGE" -t "$IMAGE_NAME:latest" $ADDITIONAL_TAG_COMMAND "$BUILD_CONTEXT" \
   && echo "Building test image..." \
-  && docker build $BUILD_ARGS --target "$TESTING_STAGE" -t "$IMAGE_NAME:$TESTING_TAG" -t "$IMAGE_NAME:testing" "$BUILD_CONTEXT"
+  && docker build $BUILD_ARGS --target "$TESTING_STAGE" -t "$IMAGE_NAME:testing" "$BUILD_CONTEXT"
 else
   echo "Building image..."
-  echo "docker build --build-arg $BUILD_ARGS -t \"$IMAGE_NAME:$GITHUB_SHA\" $ADDITIONAL_TAG_COMMAND \"$BUILD_CONTEXT"
-  docker build $BUILD_ARGS -t "$IMAGE_NAME:$GITHUB_SHA" $ADDITIONAL_TAG_COMMAND "$BUILD_CONTEXT"
+  docker build $BUILD_ARGS -t "$IMAGE_NAME:latest" $ADDITIONAL_TAG_COMMAND "$BUILD_CONTEXT"
 fi
 
 echo "Cleanup.."
-echo "Deleting older images.."
-echo "docker images | awk '{print $3}' | grep \"$IMAGE_NAME\" | grep -v \"$IMAGE_NAME:latest\|$IMAGE_NAME:testing\" | xargs docker rmi"
-docker images | awk '{print $3}' | grep "$IMAGE_NAME" | grep -v "$IMAGE_NAME:latest\|$IMAGE_NAME:testing" | xargs docker rmi
-echo "Purge unused layer cache.."
-docker builder prune -a -f
+docker system prune -f
 echo "Done."
