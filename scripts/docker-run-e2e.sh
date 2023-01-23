747ef8a0da9a516266a085088a20afc258fb4321
Federico Infanti <federico.infanti@checkout-charlie.com>
2023-01-23 17:21:39 +0100

    OPS-368: fix crashing test

--- scripts/docker-run-e2e.sh
+++ scripts/docker-run-e2e.sh
@@ -7,18 +7,18 @@ IMAGE_TAG="$2"
 DOCKER_ARGS="$3"
 COMMAND="$4"
 ENV_FILE="$5"
-SERVICE_PORT="$7"
-READINESS_TIMEOUT="$8"
+SERVICE_PORT="$6"
+READINESS_TIMEOUT="$7"
 
 HOST_PORT=8080
 CONTAINER_NAME="${IMAGE_NAME}_${IMAGE_TAG}"
 
-if [ -z "$SERVCICE_PORT" ]; then
+if [ -z "$SERVICE_PORT" ]; then
   #apt-get update
   CONTAINER_PORT=$(docker inspect --format='{{json .Config.ExposedPorts}}' "$IMAGE_NAME:$IMAGE_TAG" | jq 'keys[0]' | cut -d'/' -f1 | cut -d'"' -f2)
   echo "Detected image port: $CONTAINER_PORT"
 else
-  CONTAINER_PORT=$SERVCICE_PORT
+  CONTAINER_PORT=$SERVICE_PORT
 fi
 docker run --rm -d -p "$HOST_PORT:$CONTAINER_PORT" --name "$CONTAINER_NAME" --env-file "$ENV_FILE" $DOCKER_ARGS "$IMAGE_NAME:$IMAGE_TAG" || exit 1
 
