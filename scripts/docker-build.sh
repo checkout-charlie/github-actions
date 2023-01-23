fb2985f5031eb2dca841d23346378bef360b7a3d
Federico Infanti <federico.infanti@checkout-charlie.com>
2023-01-23 12:50:33 +0100

    Cleanup

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -11,13 +11,14 @@ TESTING_TAG="$5"
 PRODUCTION_STAGE="$6"
 TESTING_STAGE="$7"
 
+
 # Removed remote tag locally
 if [ "$MULTI_STAGE" != "false" ]
 then
 	echo "Building image..."
   docker build $BUILD_ARGS --target "$PRODUCTION_STAGE" -t "$IMAGE_NAME:latest" "$BUILD_CONTEXT" \
   && echo "Building test image..." \
-  && docker build $BUILD_ARGS --target "$TESTING_STAGE" -t "$IMAGE_NAME:testing" "$BUILD_CONTEXT"
+  && docker build $BUILD_ARGS --target "$TESTING_STAGE" -t "$IMAGE_NAME:$TESTING_TAG" "$BUILD_CONTEXT"
 else
   echo "Building image..."
   docker build $BUILD_ARGS -t "$IMAGE_NAME:latest" "$BUILD_CONTEXT"
