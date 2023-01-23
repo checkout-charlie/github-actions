f8b99111366106cda660673f7ccae572856f88f1
Federico Infanti <federico.infanti@checkout-charlie.com>
2023-01-23 13:53:11 +0100

    Add exit conditions

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-
 BUILD_CONTEXT="$1"
 BUILD_ARGS="$2"
 IMAGE_NAME="$3"
@@ -16,15 +15,15 @@ TESTING_STAGE="$7"
 if [ "$MULTI_STAGE" != "false" ]
 then
 	echo "Building image..."
-  docker build $BUILD_ARGS --target "$PRODUCTION_STAGE" -t "$IMAGE_NAME:latest" "$BUILD_CONTEXT" \
-  && echo "Building test image..." \
-  && docker build $BUILD_ARGS --target "$TESTING_STAGE" -t "$IMAGE_NAME:$TESTING_TAG" "$BUILD_CONTEXT"
+  docker build $BUILD_ARGS --target "$PRODUCTION_STAGE" -t "$IMAGE_NAME:latest" "$BUILD_CONTEXT" || exit 1
+  echo "Building test image..." \
+  docker build $BUILD_ARGS --target "$TESTING_STAGE" -t "$IMAGE_NAME:$TESTING_TAG" "$BUILD_CONTEXT" || exit 1
 else
   echo "Building image..."
-  docker build $BUILD_ARGS -t "$IMAGE_NAME:latest" "$BUILD_CONTEXT"
+  docker build $BUILD_ARGS -t "$IMAGE_NAME:latest" "$BUILD_CONTEXT" || exit 1
 fi
 
-echo "Cleanup.."
+echo "Cleanup unused Docker resources.."
 # Remove unused resources that would otherwise persist between builds by virtue of GH dependency caching
 docker system prune -f
 echo "List of the images in the runner.."
