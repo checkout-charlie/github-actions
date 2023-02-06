32dc2aa403debb26c69bdde1b3bf8f6048088bc4
Federico Infanti <federico.infanti@checkout-charlie.com>
2023-02-06 15:33:44 +0100

    Fix entrypoint

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -9,4 +9,15 @@ ENV_FILE="$4"
 RUN_ARGS="$5"
 ENTRYPOINT_OVERRIDE="$6"
 
-docker run --rm --env-file "$ENV_FILE" $RUN_ARGS "$IMAGE_NAME:$IMAGE_TAG" $ENTRYPOINT_ARG $COMMAND_ARG
+if [ -z "$ENTRYPOINT_OVERRIDE" ]; then
+  docker run --rm --env-file "$ENV_FILE" $RUN_ARGS "$IMAGE_NAME:$IMAGE_TAG" $COMMAND
+else
+  entrpoint_cmd="${$ENTRYPOINT_OVERRIDE% *}"
+  entrpoint_args="${$ENTRYPOINT_OVERRIDE#* }"
+
+  echo "First part: $entrpoint_cmd"
+  echo "Second part: $entrpoint_args"
+
+  docker run --rm --env-file "$ENV_FILE" $RUN_ARGS --entrypoint "$entrpoint_cmd" "$IMAGE_NAME:$IMAGE_TAG" $entrpoint_args $COMMAND
+fi
+
