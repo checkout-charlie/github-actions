82eb1eb62c2705db91ee3cd769d3b745dc412f20
Federico Infanti <federico.infanti@checkout-charlie.com>
2023-02-06 15:39:24 +0100

    Fix docker run

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -12,8 +12,8 @@ ENTRYPOINT_OVERRIDE="$6"
 if [ -z "$ENTRYPOINT_OVERRIDE" ]; then
   docker run --rm --env-file "$ENV_FILE" $RUN_ARGS "$IMAGE_NAME:$IMAGE_TAG" $COMMAND
 else
-  entrpoint_cmd="${$ENTRYPOINT_OVERRIDE% *}"
-  entrpoint_args="${$ENTRYPOINT_OVERRIDE#* }"
+  entrpoint_cmd="${ENTRYPOINT_OVERRIDE% *}"
+  entrpoint_args="${ENTRYPOINT_OVERRIDE#* }"
 
   echo "First part: $entrpoint_cmd"
   echo "Second part: $entrpoint_args"
