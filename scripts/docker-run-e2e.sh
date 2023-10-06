4b470b827edc6538061b9e04aee92f7199792664
Federico Infanti <federico.infanti@checkout-charlie.com>
2023-10-06 14:28:08 +0200

    ARCH-95: fix e2e tests

--- scripts/docker-run-e2e.sh
+++ scripts/docker-run-e2e.sh
@@ -9,6 +9,5 @@ SERVICE_PORT="$6"
 printenv
 yarn install
 yarn lint
-yarn add start-server-and-test
 yarn build --modern=client
 yarn start-server-and-test start "http://localhost:$SERVICE_PORT" "$COMMAND" --timeout $READINESS_TIMEOUT
