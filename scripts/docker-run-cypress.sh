1812b9bb3119ffdf02dd3de7821a65868b4d71dd
Federico Infanti <federico.infanti@checkout-charlie.com>
2023-10-07 21:13:47 +0200

    ARCH-95: add dedicated cypress test

---
+++ scripts/docker-run-cypress.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+COMMAND="$4"
+ENV_FILE="$5"
+SERVICE_PORT="$6"
+
+printenv
+yarn install
+yarn lint
+yarn build --modern=client
+yarn start-server-and-test 'yarn start' "http://localhost:$SERVICE_PORT" "$COMMAND"
