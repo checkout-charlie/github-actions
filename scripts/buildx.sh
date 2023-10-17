132dfea5ac8e14c71957d772f4e9b754625d7d28
Federico Infanti <federico.infanti@checkout-charlie.com>
2023-10-17 09:58:40 +0200

    ARCH-130 try new cache approach

---
+++ scripts/buildx.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -e
+
+BUILD_CONTEXT="$1"
+DOCKERFILE="$2"
+BUILD_ARGS="$3"
+IMAGE_NAME="$4"
+STAGE="$5"
+IS_TESTING="$6"
+
+echo "Image before build:"
+docker images
+
+
+if [ -z "$STAGE" ]; then
+  STAGE_PART=""
+else
+  STAGE_PART="--target $STAGE"
+fi
+
+# check if is_testing
+
+if [ "$IS_TESTING" = "true" ] || [ "$STAGE" = "testing" ]; then
+  IMAGE_TAG="testing"
+else
+  IMAGE_TAG="latest"
+fi
+
+echo "Building image..."
+docker buildx build $BUILD_ARGS --file "$DOCKERFILE" $STAGE_PART -t "$IMAGE_NAME:$IMAGE_TAG" "$BUILD_CONTEXT" --cache-from "type=local,src=/tmp/.buildx-cache-$IMAGE_NAME" --cache-to "type=local,dest=/tmp/.buildx-cache-new-$IMAGE_NAME,mode=max"|| exit 1
+
+echo "Post-build image list:"
+docker images
+echo "Cleanup unused Docker resources.."
+# Remove unused resources that would otherwise persist between builds by virtue of GH dependency caching
+docker rmi -f $(docker images | awk '$2 != "latest" && $2 != "testing" {print $3}') >/dev/null 2>&1 || exit 0
+docker system prune -f
+echo "Image list after cleanup:"
+docker images
+echo "List of containers:"
+docker ps -a
+echo "Done."
